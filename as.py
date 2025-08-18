5c3c0626b2f5e44a03d9ab2f99d15c17f574850b
Lasith Eshan <168047758+eshulasi333@users.noreply.github.com>
2025-08-18 09:21:51 +0530

    check word is triangular number

--- as.py
+++ as.py
@@ -1,16 +1,23 @@
-def palindoramic(n):
-    n=int(n)
-    m=int(str(n)[-1::-1])
-    if m==n:
-        return True
-def binary(k):
-    re=bin(k)
-    er=re[2:]
-    return er
-sum=0
-for x in range(1000000):
-    if palindoramic(x):
-        res=binary(x)
-        if palindoramic(res):
-            sum+=x
-print(sum)
+def tri(m,l):
+    a=m[-1]
+    b=m[-2]
+    diff=a-b+1
+    tu=a+diff
+    m.append(tu)
+    if tu<l:
+        return tri(m,l)
+    return m
+se=[1,3,6,10]
+so=input(">").strip().split(",")
+count=0
+val=0
+for x in so:
+    tr=x[1:-1]
+    for t in tr:
+        val+=ord(t.lower())-ord("a")+1
+    if val>se[-1]:
+        ty=tri(se,val)
+    if val in se:
+        count+=1
+    val=0
+print(count)
